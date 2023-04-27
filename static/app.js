74edccc209ae5f52a1ad6846a5c36f67180b4c15
nicholasmullins <nick.mullins@generalassemb.ly>
2023-04-27 00:04:17 -0400

    added function to close mobile keyboard on Enter

--- static/app.js
+++ static/app.js
@@ -43,3 +43,16 @@ const createColorBoxes = (colors, parent) => {
     }
 
 }
+
+const hideMobileKeyboardOnReturn = (element) => {
+    element.addEventListener('keyup', (keyboardEvent) => {
+        const key = keyboardEvent.code || keyboardEvent.keyCode;
+        if (key === 'Enter' || key === 13) {
+            element.blur();
+        }
+    });
+};
+
+document.querySelectorAll('#search').forEach((element) => {
+    hideMobileKeyboardOnReturn(element);
+}); 
